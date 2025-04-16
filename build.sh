02a9450bc219ca5eca5f230b220707dbcc7a833d
maharsh_nayak <64581149+Maharsh-Nayak@users.noreply.github.com>
2025-04-16 19:51:06 +0530

    hosting changes

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
 
-# Install dependencies
+# Install Python dependencies
 pip install -r requirements.txt
 
 # Collect static files
